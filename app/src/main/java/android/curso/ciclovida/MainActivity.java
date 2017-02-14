5c258a1e79d4b80dfb83abf37d7163a48cf50d4f
Andres Vasquez <andres.vasquez.a@hotmail.com>
2017-02-14 08:52:43 -0400

    Subiendo los archivos por primera vez. First commit.

---
+++ app/src/main/java/android/curso/ciclovida/MainActivity.java
@@ -0,0 +1,60 @@
+package android.curso.ciclovida;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Prueba con GIT
+    //Aca comienza la app
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("Estado: ", "MainActivity.Create");
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Log.d("Estado: ","MainActivity.Pause");
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        Log.w("Estado: ","MainActivity.Stop");
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Log.d("Estado: ","MainActivity.Resume");
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+        Log.d("Estado: ","MainActivity.Restart");
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.d("Estado: ","MainActivity.Destroy");
+    }
+
+    public void clickAbrir(View view){
+        Log.d("Estado: ","Click en boton");
+        Intent intent=new Intent(this,SecondActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/android/curso/ciclovida/SecondActivity.java
@@ -0,0 +1,51 @@
+package android.curso.ciclovida;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class SecondActivity extends AppCompatActivity {
+
+    //Aca comienza la app
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Log.d("Estado: ", "SecondActivity.Create");
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        Log.d("Estado: ","SecondActivity.Pause");
+    }
+
+    @Override
+    protected void onStop()
+    {
+        super.onStop();
+        Log.d("Estado: ","SecondActivity.Stop");
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Log.d("Estado: ","SecondActivity.Resume");
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+        Log.d("Estado: ","SecondActivity.Restart");
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        Log.d("Estado: ","SecondActivity.Destroy");
+    }
+}
